f19ac6c21e54a4a52d249abc8525c7d69c67adc9
BackdoorBDG <luckyjackson2024245@gmail.com>
2025-03-26 18:20:53 -0400

    Create OpenAiResponse.swift

---
+++ iOS/Common/OpenAiResponse.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+/// Model for OpenAI API response
+struct OpenAIResponse: Codable {
+    let choices: [Choice]
+    
+    struct Choice: Codable {
+        let message: Message
+    }
+    
+    struct Message: Codable {
+        let content: String
+    }
+}
