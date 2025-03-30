7bfec9448e43504e8eb01feb9eb2ec85b17184f9
Backdoor-App-Signer <wedasfwedas@rambler.ru>
2025-03-29 23:16:03 -0400

    Update DisplayCollectionTableViewCell.swift

--- iOS/Views/Settings/Display/DisplayCollectionTableViewCell.swift
+++ iOS/Views/Settings/Display/DisplayCollectionTableViewCell.swift
@@ -6,14 +6,6 @@
 // Backdoor App Signer is proprietary software. You may not use, modify, or distribute it except as expressly permitted under the terms of the Proprietary Software License.
 //
 
-//
-//  DisplayTintViewCollectionCell.swift
-//  pointercrate
-//
-//  Created by samara on 4/3/24.
-//  Copyright (c) 2024 Samara M (khcrysalis)
-//
-
 import Foundation
 import UIKit
 
