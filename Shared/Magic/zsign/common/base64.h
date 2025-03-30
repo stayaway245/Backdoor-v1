d1961812c2d817637dfc688757fd532139d6a9df
MentatBot <160964065+MentatBot@users.noreply.github.com>
2025-03-30 04:57:44 +0000

    Fix duplicate license headers in 1 files

--- Shared/Magic/zsign/common/base64.h
+++ Shared/Magic/zsign/common/base64.h
@@ -5,6 +5,12 @@
 <<<<<<< HEAD
  * Backdoor App Signer is proprietary software. You may not use, modify, or distribute it except as expressly permitted under the terms of the Proprietary Software License.
 
+ *
+ * Copyright (C) 2025 BDG
+ *
+<<<<<<< HEAD
+ * Backdoor App Signer is proprietary software. You may not use, modify, or distribute it except as expressly permitted under the terms of the Proprietary Software License.
+
 /*
  * Proprietary Software License Version 1.0
  *
