971e4f9ac0d9bde4f060b38ba05ac585d4e4cc9a
Backdoor-App-Signer <wedasfwedas@rambler.ru>
2025-03-29 23:18:02 -0400

    Update LicenseViewController.swift

--- iOS/Views/Settings/About/LicenseViewController.swift
+++ iOS/Views/Settings/About/LicenseViewController.swift
@@ -6,13 +6,6 @@
 // Backdoor App Signer is proprietary software. You may not use, modify, or distribute it except as expressly permitted under the terms of the Proprietary Software License.
 //
 
-//
-//  LicenseViewController.swift
-//  backdoor
-//
-//  Created by samara on 28.10.2024.
-//
-
 import UIKit
 
 class LicenseViewController: UIViewController {
