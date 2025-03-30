b4bae69f0319b51fc35db8452766e957e70580d1
Backdoor-App-Signer <wedasfwedas@rambler.ru>
2025-03-29 23:17:29 -0400

    Update IconsListTableViewCell.swift

--- iOS/Views/Settings/AppIcon/IconsListTableViewCell.swift
+++ iOS/Views/Settings/AppIcon/IconsListTableViewCell.swift
@@ -6,14 +6,6 @@
 // Backdoor App Signer is proprietary software. You may not use, modify, or distribute it except as expressly permitted under the terms of the Proprietary Software License.
 //
 
-//
-//  IconsListTableViewCell.swift
-//  backdoor
-//
-//  Created by samara on 8/11/24.
-//  Copyright (c) 2024 Samara M (khcrysalis)
-//
-
 import UIKit
 
 class IconsListTableViewCell: UITableViewCell {
