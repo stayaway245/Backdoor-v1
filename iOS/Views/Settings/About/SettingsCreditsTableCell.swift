8df3759bbbac7c7e7a3b61ae03a7e1e095998b1d
Backdoor-App-Signer <wedasfwedas@rambler.ru>
2025-03-29 23:18:24 -0400

    Update SettingsCreditsTableCell.swift

--- iOS/Views/Settings/About/SettingsCreditsTableCell.swift
+++ iOS/Views/Settings/About/SettingsCreditsTableCell.swift
@@ -6,14 +6,6 @@
 // Backdoor App Signer is proprietary software. You may not use, modify, or distribute it except as expressly permitted under the terms of the Proprietary Software License.
 //
 
-//
-//  SettingsCreditsTableCell.swift
-//  backdoor
-//
-//  Created by samara on 7/10/24.
-//  Copyright (c) 2024 Samara M (khcrysalis)
-//
-
 import UIKit
 
 struct CreditsPerson: Codable {
